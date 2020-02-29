3d8484933bf9096ca5a19149841ad8133ce473ba
rpizarrog <rpizarro@itdurango.edu.mx>
2020-02-28 18:05:49 -0600

    Primeros cambios

---
+++ scripts/cargar datos coronavirus y recordar R.R
@@ -0,0 +1,11 @@
+# Verificar en que directorio estamos
+# cargar los datos de coronavirus
+# Ver cuantos registros tenemos
+# summary(datos)
+
+print ("Hola Mundo")
+
+# Cargar las librerías
+library(readr)
+
+
