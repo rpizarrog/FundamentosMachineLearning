8429577a710449d1fdc5e324bedad9a5e21a2ab3
rpizarrog <rpizarro@itdurango.edu.mx>
2020-02-28 18:49:07 -0600

    Cuantos confirmados y cuantos decesos, %

--- scripts/cargar datos coronavirus y recordar R.R
+++ scripts/cargar datos coronavirus y recordar R.R
@@ -7,8 +7,40 @@ print ("Hola Mundodfgdfg ")
 
 # Cargar las librerías
 library(readr)
+library(dplyr)
 
 print ("Cargar Datos")
 
 cat("probando un nuevo print")
+
+
+
+# Cargar los datos
+datos <- read.csv("datos/covid_19_data.csv")
+datos
+
+summary(datos)
+str(datos)
+
+unique(datos$Country.Region)
+
+
+# ¿Cuángos casos confirmados ?
+sum(datos$Confirmed)
+
+# ¿cántos desscesos?
+sum(datos$Deaths)
+
+# ¿Porcentaje, descesos confirmados
+sum(datos$Confirmed / sum(datos$Deaths))
+
+paste(round(sum(datos$Confirmed / sum(datos$Deaths)),2), " % ")
+
+
+
+
+
+
+
+
 
