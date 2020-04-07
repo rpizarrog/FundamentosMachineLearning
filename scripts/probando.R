b00bc7c6df4210d42cac93a58cf9bbcc1a2c3aa0
rpizarrog <rpizarro@itdurango.edu.mx>
2020-04-07 11:37:18 -0500

    Cambios varios

---
+++ scripts/probando.R
@@ -0,0 +1,12 @@
+# Probando 
+datos = data.frame(
+  name=c("Tony","James","Sara","Alice","David","Angie","Don","Faith","Becky","Jenny",
+         "Kristi","Neil","Brandon","Kara","Kendra","Liz","Gina","Amber","Alice","George"),
+  state=c("KS","IA","CA","FL","MI","CO","KA","CO","KS","CA","MN","FL","NM","MS","GA",
+          "IA","IL","ID","NY","NJ"),
+  gender=c("M","M","F","F","F","M","F","M","F","F","F","M","M","F","F","F","F","F","F","M"),
+  marital_status=c("M","S","S","S","M","M","S","M","S","M","M","S","S","S","M","M","S","M","S","M"),
+  credit=c("good","good","poor","fair","poor","fair","fair","fair","good","fair",
+           "good","good","poor","fair","poor","fair","fair","fair","good","fair"),
+  owns_home=c(0,1,0,0,1,0,1,1,1,1,0,1,0,0,1,0,1,1,1,1),
+  cost=c(500,200,300,150,200,300,400,450,250,150,500,200,300,150,200,300,400,450,250,150))
