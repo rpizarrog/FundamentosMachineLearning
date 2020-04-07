d3ee7f9767e94fb5b3cbbb9386935db84156587e
rpizarrog <rpizarro@itdurango.edu.mx>
2020-04-07 11:35:02 -0500

    Probando Shiny

--- ProbandoShiny/app.R
+++ ProbandoShiny/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Datos del  Geyser Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
